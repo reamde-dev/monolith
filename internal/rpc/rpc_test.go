3f40fad656ef60d196a344137f674aeea91ddd8f
George Antoniadis <george@noodles.gr>
2023-10-16 00:11:49 +0100

    feat(rpc): support multiple connections and requests per connection (#3)

--- internal/rpc/rpc_test.go
+++ internal/rpc/rpc_test.go
@@ -7,28 +7,33 @@ import (
 )
 
 func TestClientServer(t *testing.T) {
-	server := &Server{
-		Address: ":8081",
-		Handler: func(request Request) Response {
-			return Response{
-				Body: []byte(fmt.Sprintf("Topic: %s, Payload: %s", request.Topic, request.Body)),
-			}
-		},
+	// Start multiple servers
+	serverAddresses := []string{":8081", ":8082", ":8083"}
+	for _, address := range serverAddresses {
+		server := &Server{
+			Address: address,
+			Handler: func(request Request) Response {
+				return Response{
+					Body: []byte(fmt.Sprintf("Topic: %s, Payload: %s", request.Topic, request.Body)),
+				}
+			},
+		}
+		go server.Listen() // start each server in a goroutine
 	}
-	go server.Listen() // start server in a goroutine
 
-	// Allow the server a moment to start up
+	// Allow the servers a moment to start up
 	time.Sleep(1 * time.Second)
 
-	client := &Client{
-		ServerAddress: "localhost:8081",
-	}
+	client := &Client{}
 
+	// We will have one test for each topic for each server
 	tests := []struct {
-		request  Request
-		expected Response
+		serverAddress string
+		request       Request
+		expected      Response
 	}{
 		{
+			serverAddress: "localhost:8081",
 			request: Request{
 				Topic: "topic1",
 				Body:  []byte("Payload for topic1"),
@@ -38,6 +43,7 @@ func TestClientServer(t *testing.T) {
 			},
 		},
 		{
+			serverAddress: "localhost:8081", // Same server to test multiple requests
 			request: Request{
 				Topic: "topic2",
 				Body:  []byte("Payload for topic2"),
@@ -46,11 +52,31 @@ func TestClientServer(t *testing.T) {
 				Body: []byte("Topic: topic2, Payload: Payload for topic2"),
 			},
 		},
+		{
+			serverAddress: "localhost:8082",
+			request: Request{
+				Topic: "topic1",
+				Body:  []byte("Payload for topic1"),
+			},
+			expected: Response{
+				Body: []byte("Topic: topic1, Payload: Payload for topic1"),
+			},
+		},
+		{
+			serverAddress: "localhost:8083",
+			request: Request{
+				Topic: "topic1",
+				Body:  []byte("Payload for topic1"),
+			},
+			expected: Response{
+				Body: []byte("Topic: topic1, Payload: Payload for topic1"),
+			},
+		},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.request.Topic, func(t *testing.T) {
-			response, err := client.SendRequest(tt.request)
+		t.Run(fmt.Sprintf("%s-%s", tt.serverAddress, tt.request.Topic), func(t *testing.T) {
+			response, err := client.SendRequest(tt.serverAddress, tt.request)
 			if err != nil {
 				t.Fatalf("Expected no error, but got: %v", err)
 			}
