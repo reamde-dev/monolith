de1a7bb3c4f13216c5fccf9b08988305d193832e
George Antoniadis <george@noodles.gr>
2023-10-16 19:08:37 +0100

    feat(proto/monolith): rename ClientInfo to AccountInfo (#4)

--- internal/client/client.go
+++ internal/client/client.go
@@ -8,14 +8,14 @@ import (
 )
 
 type Client struct {
-	Info      *monolith.ClientInfo
-	rpcClient *rpc.Client
+	AccountInfo *monolith.AccountInfo
+	rpcClient   *rpc.Client
 }
 
-func NewClient(info *monolith.ClientInfo) *Client {
+func NewClient(accountInfo *monolith.AccountInfo) *Client {
 	return &Client{
-		Info:      info,
-		rpcClient: &rpc.Client{},
+		AccountInfo: accountInfo,
+		rpcClient:   &rpc.Client{},
 	}
 }
 
@@ -24,7 +24,7 @@ func (c *Client) SendRequest(topic string, body []byte) (rpc.Response, error) {
 		Topic: topic,
 		Body:  body,
 	}
-	addr := c.Info.Brokers[0].Peers[0].Addresses[0]
+	addr := c.AccountInfo.Brokers[0].Peers[0].Addresses[0]
 	host := fmt.Sprintf("%s:%d", addr.Address, addr.Port)
 	return c.rpcClient.SendRequest(host, request)
 }
