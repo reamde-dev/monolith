d765f74953532c289e6604066fe6cb873613fa6c
George Antoniadis <george@noodles.gr>
2023-10-16 19:15:18 +0100

    feat(proto/monolith): simplify ProviderInfo (#4)

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -10,19 +10,31 @@ import (
 // BrokerServer handles incoming requests from clients and forwards them to providers.
 type BrokerServer struct {
 	BrokerInfo    *monolith.BrokerInfo
-	ProviderInfos map[string]*monolith.ProviderInfo
+	ProviderInfos []*monolith.ProviderInfo
 	rpcServer     *rpc.Server
 }
 
 func (b *BrokerServer) Handler(request rpc.Request) rpc.Response {
-	provider, ok := b.ProviderInfos[request.Topic]
-	if !ok {
+	var foundProviderInfo *monolith.ProviderInfo
+
+findProviderInfo:
+	for _, providerInfo := range b.ProviderInfos {
+		for _, topic := range providerInfo.Topics {
+			if topic != request.Topic {
+				continue
+			}
+			foundProviderInfo = providerInfo
+			break findProviderInfo
+		}
+	}
+
+	if foundProviderInfo == nil {
 		return rpc.Response{
 			Error: fmt.Sprintf("No provider found for topic: %s", request.Topic),
 		}
 	}
 
-	addr := provider.Peer.Addresses[0]
+	addr := foundProviderInfo.Peer.Addresses[0]
 	host := fmt.Sprintf("%s:%d", addr.Address, addr.Port)
 
 	client := &rpc.Client{}
